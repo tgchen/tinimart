4a28acca155a06839c6bf886a7dc35b958441527
TC Chen <tgchen@wynetech.com>
2011-10-06 20:54:14 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+gem 'sqlite3'
+gem 'gravatar_image_tag', '0.1.0'
+gem 'will_paginate', '3.0.pre'
+gem 'nokogiri'
+
+ group :development do
+   gem 'rspec-rails', '2.0.0'
+   gem 'annotate'
+   gem 'faker', '0.3.1'
+end
+
+group :test do
+  gem 'rspec', '2.0.0'
+  gem 'spork'
+  gem 'webrat', '0.7.1'
+  gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinimart::Application.config.secret_token = 'af8a58818005f9513cf413cc930114da4685b85ccb8b843f38b38f1e951b136d595dbd9465ca25909ce823a8cc97b9326eb844ee774c52a2a878599660b44e6d'
